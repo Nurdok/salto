8652d9d6a395020c4255c813d068701680d21260
Amir Rachum <amir.rachum@salto.io>
2024-09-02 16:37:58 +0300

    I did some unspeakable things to global value sets that I should probably never mention to anyone

--- packages/salesforce-adapter/src/filters/global_value_sets.ts
+++ packages/salesforce-adapter/src/filters/global_value_sets.ts
@@ -5,12 +5,22 @@
  *
  * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
  */
-import { Element, ObjectType, Field, ReferenceExpression, ElemID, isObjectType } from '@salto-io/adapter-api'
+import {
+  Element,
+  ObjectType,
+  Field,
+  ReferenceExpression,
+  ElemID,
+  isObjectType,
+  Values,
+  Value, MapType,
+} from '@salto-io/adapter-api'
 import { multiIndex } from '@salto-io/lowerdash'
 import { LocalFilterCreator } from '../filter'
 import { VALUE_SET_FIELDS } from '../constants'
 import { isCustomObject, apiName } from '../transformers/transformer'
-import { isInstanceOfType, buildElementsSourceForFetch } from './utils'
+import { isInstanceOfType, buildElementsSourceForFetch, isInstanceOfTypeSync } from './utils'
+import { naclCase } from '@salto-io/adapter-utils'
 
 export const GLOBAL_VALUE_SET = 'GlobalValueSet'
 export const CUSTOM_VALUE = 'customValue'
@@ -40,6 +50,23 @@ const filterCreator: LocalFilterCreator = ({ config }) => ({
    * @param elements the already fetched elements
    */
   onFetch: async (elements: Element[]): Promise<void> => {
+    elements.filter(isInstanceOfTypeSync(GLOBAL_VALUE_SET)).forEach(gvsInstance => {
+        const valueMap: Values = {}
+        if (gvsInstance.value.customValue === undefined) {
+          return
+        }
+        if (!Array.isArray(gvsInstance.value.customValue)) {
+          gvsInstance.value.customValue = [gvsInstance.value.customValue]
+        }
+        gvsInstance.value.customValue.forEach((customValue: Value) => {
+          valueMap[naclCase(customValue.fullName)] = customValue
+        })
+        gvsInstance.value.customValue = valueMap
+        if (gvsInstance.refType.type?.fields?.customValue?.refType?.type !== undefined) {
+          gvsInstance.refType.type.fields.customValue.refType.type = new MapType(
+            gvsInstance.refType.type.fields.customValue.refType?.type)
+        }
+    })
     const referenceElements = buildElementsSourceForFetch(elements, config)
     const valueSetNameToRef = await multiIndex.keyByAsync({
       iter: await referenceElements.getAll(),
